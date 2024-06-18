9809c373439201a476d499e22029b7df28d7d3b8
Deobaba <adexsquare4192@gmail.com>
2024-06-18 12:42:45 +0100

    issue

--- issue-tracker/app/NavBar.tsx
+++ issue-tracker/app/NavBar.tsx
@@ -1,8 +1,13 @@
+ 'use client';
  import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 import React from 'react'
 import { FaBug } from "react-icons/fa";
+import classNames from 'classnames';
  
  const NavBar = () => {
+
+    const currentPath = usePathname()
     
     const links = [
         {label:'Dashboard', href:'/'},
@@ -14,7 +19,13 @@ import { FaBug } from "react-icons/fa";
        <Link href='/'><FaBug/></Link>
        <ul className='flex space-x-6'>
         {links.map(link =>
-         <Link key={link.href} className='text-zinc-500 hover:text-zinc-900  transition-colors' href={link.href}>
+         <Link key={link.href} className={
+            classNames({
+                'text-zinc-900':link.href==currentPath,
+                'text-zinc-500': link.href !== currentPath,
+                'hover:text-zinc-800 transition-colors':true
+            })
+         } href={link.href}>
             {link.label}
          </Link>
          )
